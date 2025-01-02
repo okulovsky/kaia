19f47e4eec8605bcf83c19f5686a67cc041d2704
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2025-01-02 15:49:10 +0100

    Old folders are removed

--- kaia_tests/test_eaglesong/test_telegram/test_tg_command.py
+++
@@ -1,8 +0,0 @@
-from kaia.eaglesong.drivers.telegram import TgCommand
-from unittest import TestCase
-
-class TgCommandMockTestCase(TestCase):
-    def test_mock(self):
-        cmd = TgCommand.mock().send_message(text='test', reply_to_message_id=123) #type: TgCommand
-        self.assertEqual('send_message', cmd.bot_method)
-        self.assertDictEqual({'text': 'test', 'reply_to_message_id': 123}, cmd.kwargs)
--- zoo/assistant/nutrition/app/statistics.py
+++
@@ -1,2 +0,0 @@
-
-#Carbohydrates provide 4 calories per gram, protein provides 4 calories per gram, and fat provides 9 calories per gram.
