280518110d74bd922e444da04e0f073eb2c60fd1
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2025-01-02 16:00:13 +0100

    kaia.demo moved from root to kaia

---
+++ kaia/demo/kaia_core_service.py
@@ -0,0 +1,69 @@
+from kaia import skills
+from .avatar import characters
+from kaia.kaia import KaiaCoreService, KaiaCoreServiceSettings, Message, KaiaAssistant
+from pathlib import Path
+from brainbox import File, ControllersSetup
+from .app import KaiaApp
+from kaia.dub.languages.en import *
+from brainbox.deciders import Whisper, OpenTTS, RhasspyKaldi
+
+
+class CommonIntents(TemplatesCollection):
+    stop = Template('Stop','Cancel')
+
+
+class DemoCoreService(KaiaCoreService):
+    def __init__(self,
+                 settings: KaiaCoreServiceSettings
+                 ):
+        super().__init__(settings)
+
+
+    def create_assistant(self):
+        skills_list = []
+
+        skills_list.append(skills.EchoSkill())
+        skills_list.append(skills.PingSkill())
+
+        skills_list.append(skills.DateSkill())
+        skills_list.append(skills.TimeSkill())
+        # The latitude of Alexanderplatz, Berlin, Germany is 52.521992, and the longitude is 13.413244.
+        weather_settings = skills.weather.WeatherSettings(52.521992, 13.413244, 'Europe/Berlin')
+        skills_list.append(skills.weather.WeatherSkill(weather_settings))
+        skills_list.append(skills.JokeSkill())
+
+        timer_audio = File.read(Path(__file__).parent/'files/sounds/alarm.wav')
+        timer_audio.text = '*alarm ringing*'
+        timer_register = skills.NotificationRegister(timer_audio, Message(Message.Type.System, '*alarm stopped*'))
+        skills_list.append(skills.TimerSkill(timer_register))
+        skills_list.append(skills.NotificationSkill([timer_register], pause_between_alarms_in_seconds=10, volume_delta=0.2))
+
+        skills_list.append(skills.ChangeImageSkill(self.settings.avatar_api))
+        skills_list.append(skills.VolumeSkill(0.2))
+        skills_list.append(help:=skills.HelpSkill())
+        skills_list.append(skills.LogFeedbackSkill())
+        skills_list.append(character_skill:=skills.ChangeCharacterSkill(characters, self.settings.avatar_api))
+        skills_list.append(skills.InitializationSkill(character_skill))
+
+        assistant = KaiaAssistant(skills_list)
+        assistant.raise_exceptions = False
+        help.assistant = assistant
+        assistant.additional_intents.extend(CommonIntents.get_templates())
+        return assistant
+
+
+def set_core_service(app: KaiaApp):
+    setup = ControllersSetup((
+        ControllersSetup.Instance(RhasspyKaldi),
+        ControllersSetup.Instance(OpenTTS),
+        ControllersSetup.Instance(Whisper, None, 'base')
+    ))
+    settings = KaiaCoreServiceSettings(
+        avatar_api=app.avatar_api,
+        audio_api=app.audio_control_api,
+        brainbox_api=app.brainbox_api,
+        kaia_api=app.kaia_api,
+        brainbox_setup=setup,
+    )
+    app.kaia_core = DemoCoreService(settings)
+
---
+++ kaia/demo/run_demo.py
@@ -0,0 +1,24 @@
+from kaia.demo import *
+from kaia.common import Loc
+
+
+mic_settings = MicSettings(
+    mic_device_index=-1,
+    sample_rate=16000,
+    silence_level=2000
+)
+
+
+if __name__ == '__main__':
+    app = KaiaApp(folder=Loc.data_folder/'demo')
+    set_brainbox_service_and_api(app)
+    set_streaming_service_and_api_address(app)
+    set_avatar_service_and_api(app)
+    set_web_service_and_api(app, 'console')
+    set_audio_control_service_and_api(app, 'console', mic_settings)
+    set_core_service(app)
+
+    fork_app = app.get_fork_app(False)
+    fork_app.run()
+
+
