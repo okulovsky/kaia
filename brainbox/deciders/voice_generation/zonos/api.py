96c662d97c36a985bb90dbeae9c35a79476fbe91
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2025-02-23 17:10:05 +0100

    Api implemented

--- brainbox/deciders/voice_generation/zonos/api.py
+++ brainbox/deciders/voice_generation/zonos/api.py
@@ -1,8 +1,7 @@
-from ....framework import DockerWebServiceApi
+from ....framework import DockerWebServiceApi, FileLike, File
 import requests
 from .settings import ZonosSettings
 from .controller import ZonosController
-
 
 
 class Zonos(DockerWebServiceApi[ZonosSettings, ZonosController]):
@@ -10,16 +9,34 @@ class Zonos(DockerWebServiceApi[ZonosSettings, ZonosController]):
         super().__init__(address)
 
 
-    def echo(self, argument: str):
-        result = requests.post(
-            self.endpoint('/echo'),
-            json = dict(
-                argument=argument,
+    def train(self, speaker: str, sample_file: FileLike.Type):
+        with FileLike(sample_file, self.cache_folder) as content:
+            reply = requests.post(
+                self.endpoint('/train/'+speaker),
+                files=(
+                    (FileLike.get_name(sample_file), content),
+                )
+            )
+            if reply.status_code != 200:
+                raise ValueError(f"Endpoint returned an error\n{reply.text}")
+            return reply.text
+
+    def voiceover(self, text: str, speaker: str, language: str = 'en-us'):
+        reply = requests.post(
+            self.endpoint('/voiceover'),
+            json=dict(
+                text=text,
+                speaker=speaker,
+                language = language
             )
         )
-        if result.status_code!=200:
-            raise ValueError(f"Endpoint /echo returned unexpected status code {result.status_code}\n{result.text}")
-        return result.json()
+        if reply.status_code != 200:
+            raise ValueError(f"Endpoint returned an error\n{reply.text}")
+        return File(
+            self.current_job_id+'.output.wav',
+            reply.content,
+            File.Kind.Audio
+        )
 
     Settings = ZonosSettings
     Controller = ZonosController
