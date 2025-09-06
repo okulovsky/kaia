19cf77a04dbd7612f3403135bcdb17f7a27a3045
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2025-09-06 14:04:03 +0200

    Phonix
    - Sox output is supported (works better at Raspberry pie)

--- phonix/daemon/__init__.py
+++ phonix/daemon/__init__.py
@@ -1,4 +1,4 @@
 from .inputs import FakeInput, PyAudioInput
-from .outputs import FakeOutput, PyAudioOutput
+from .outputs import FakeOutput, PyAudioOutput, SoxAudioOutput
 from .processing import *
 from .daemon import *
