b884cb8679a344947d9a6fd84496bb4f1ecf6da3
yellooot <njkzrhjkz@gmail.com>
2025-12-09 23:52:52 +0500

    fix: hf repo with correct eos token id

--- brainbox/deciders/text/llama_lora_server/settings.py
+++ brainbox/deciders/text/llama_lora_server/settings.py
@@ -7,7 +7,7 @@ from .model import HFModel
 @dataclass
 class LlamaLoraServerSettings:
     connection = ConnectionSettings(20403)
-    gguf_model = HFModel("unsloth/gemma-3-270m-it-GGUF", "models", "gemma-3-270m-it-Q8_0.gguf")
+    gguf_model = HFModel("ggml-org/gemma-3-270m-it-GGUF", "models", "gemma-3-270m-it-Q8_0.gguf")
     self_test_lora_adapters = [
         HFModel("yellooot/gemma-3-270m-it-kaia-loras", "lora_adapters", "timer_self_test.gguf"),
         HFModel("yellooot/gemma-3-270m-it-kaia-loras", "lora_adapters", "nutrition_self_test.gguf"),
