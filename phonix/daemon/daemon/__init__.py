39f9eab9a83b762f9e69ec693483439f1826a4a1
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2026-04-05 10:51:30 +0200

    RM phonix

--- phonix/daemon/daemon/__init__.py
+++
@@ -1,4 +0,0 @@
-from .daemon import PhonixDeamon
-from .events import SoundPlayStarted, MicStateChangeReport
-from .utils import AvatarFileRetriever, ServerFileRetriever, FolderFileRetriever
-from .volume_deamon import VolumeDeamon
--- phonix/daemon/processing/__init__.py
+++
@@ -1,8 +0,0 @@
-from .core import IUnit, State, MicState, UnitInput, SystemSoundType, SystemSoundCommand, IMonitor
-from .porcupine_wake_word_unit_old_version import PorcupineWakeWordUnitOldVersion
-from .porcupine_wake_word_unit_new_version import PorcupineWakeWordUnitNewVersion
-from .recording_unit import RecordingUnit
-from .silence_margin_unit import SilenceMarginUnit, SilenceLevelReport
-from .too_long_open_mic_unit import TooLongOpenMicUnit
-from .level_reporting_unit import LevelReportingUnit, SoundLevelReport
-from .mic_recording import MicRecordingUnit
