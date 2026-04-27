c1c6f0ee016b43768d9e5f83aa465bc0b3c42224
nick <nick@nicks-MacBook-Air.local>
2026-04-27 11:29:13 +0500

    update expand_dataset

--- kaia/tools/expand_dataset.py
+++ kaia/tools/expand_dataset.py
@@ -3,7 +3,7 @@ from chara.paraphrasing.expand import ExpandPipeline, ExpandPipelineCache
 from foundation_kaia.misc import Loc
 from yo_fluq import FileIO
 
-records = FileIO.read_pickle(Loc.data_folder / 'result')
+records = FileIO.read_pickle(Loc.data_folder / 'intent_paraphrases_cache' / 'result')
 
 cache = ExpandPipelineCache(Loc.data_folder / 'expand_cache')
 ExpandPipeline(n_values=5, language='ru')(
