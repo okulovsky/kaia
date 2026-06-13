0306282986eaaf622ac5aefbafb8ee522f47f12e
Okulovsky Yuri <yuri.okulovsky@gmail.com>
2026-06-13 17:46:29 +0200

    Jinja prompter is now in Chara, and supports e.g. jinja templates from one folder importing scripts from another folder

--- chara/common/tools/llm/__init__.py
+++ chara/common/tools/llm/__init__.py
@@ -1,3 +1,4 @@
 from .bullet_point_divider import BulletPointDivider
 from .prompt_task_builder import PromptTaskBuilder
 from .json_parser import parse_json
+from .jinja_prompter import JinjaPrompter
